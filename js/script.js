71d6e8e3abd37d18a3c2496efe851d9bbf1ce4ce
lil-melon <133168636+lil-melon@users.noreply.github.com>
2024-10-30 15:39:12 -0700

    Include labels in console output

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("empForm")
 form.addEventListener("submit",(event) =>{
     event.preventDefault()
-    for(let i = 0; i < event.target.length; i++){
-        console.log(event.target[i].value)
+    for(let i = 0; i < event.target.length-1; i++){ //exclude submit button
+        console.log(event.target[i].labels[0].innerHTML + ": " + event.target[i].value)
     }
 })
